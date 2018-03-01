0920b4a9b42ed411e0ad6dda29b07852852577a2
Brentwood Reid <bwoodreid@gmail.com>
2018-03-01 09:53:20 -0800

    Add model tests for expense

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to :amount }
+  it { should respond_to :date }
+  it { should respond_to :category_id }
+  it { should respond_to :vendor_id }
+  it { should respond_to :comment }
+  it { should respond_to :user_id }
 end
