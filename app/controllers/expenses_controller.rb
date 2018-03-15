c8e019769cd2ec04adf6009c1d8bb588e3fb127b
Brentwood Reid <bwoodreid@gmail.com>
2018-03-15 14:02:05 -0700

    Order categories by popularity

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,7 +8,7 @@ class ExpensesController < ApplicationController
     @expenses = Expense.all.order("date DESC")
     @expenses_this_month = Expense.where('date >= ? AND date <= ?', DateTime.now.beginning_of_month, DateTime.now.end_of_month).order("date DESC")
     if current_user
-      @categories = current_user.categories
+      @categories = current_user.categories.order('description ASC')
     else
       @categories = []
     end
