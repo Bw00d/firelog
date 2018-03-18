e1be7bbbeacd0095fb41939276087289c2ca56b6
Brentwood Reid <bwoodreid@gmail.com>
2018-03-18 07:54:14 -0700

    Change js add line and remove require statements for charts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require Chart.bundle
-//= require chartkick
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
