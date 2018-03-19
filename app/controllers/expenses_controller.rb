ad62f9ca8f888263cbee655e2dadc58b8d62542d
Brentwood Reid <bwoodreid@gmail.com>
2018-03-19 08:58:27 -0700

    Add delete expense link

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -66,7 +66,7 @@ class ExpensesController < ApplicationController
   def destroy
     @expense.destroy
     respond_to do |format|
-      format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
+      format.html { redirect_to expenses_url }
       format.json { head :no_content }
     end
   end
