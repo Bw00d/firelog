c5f0d1b49f669bef2f34fcfb638978edd9eb2358
Brentwood Reid <bwoodreid@gmail.com>
2018-03-26 11:46:07 -0700

    Fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-  :s3_region => ENV['AWS_REGION']
+  :s3_region => ENV['AWS_REGION'],
   
   :url =>':s3_domain_url',
   :path => '/:class/:attachment/:id_partition/:style/:filename',
