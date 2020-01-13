e6ecd947e57bf6ed4c6040f62531c5df99fc10c2
Brentwood Reid <bwoodreid@gmail.com>
2020-01-12 16:20:57 -0800

    Temporarily display 2019 expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,25 +7,25 @@ class ExpensesController < ApplicationController
   # GET /expenses.json
   def index
     @expense = Expense.new
-      if current_user
+    if current_user
+      @expenses = @current_user_expenses.where('date >= ? AND date <= ?',
+                                         DateTime.now.beginning_of_month,
+                                 DateTime.now.end_of_month).order("date DESC")
+      @yearly_expenses = @current_user_expenses.where('date >= ? AND date <= ?',
+                                                DateTime.now.beginning_of_year,
+                                                DateTime.now).order("date DESC")
+      if params[:search]
+        @expenses = @current_user_expenses.search(params[:search]).order("date DESC")
+      else
         @expenses = @current_user_expenses.where('date >= ? AND date <= ?',
                                            DateTime.now.beginning_of_month,
                                    DateTime.now.end_of_month).order("date DESC")
-        @yearly_expenses = @current_user_expenses.where('date >= ? AND date <= ?',
-                                                  DateTime.now.beginning_of_year,
-                                                  DateTime.now).order("date DESC")
-        if params[:search]
-          @expenses = @current_user_expenses.search(params[:search]).order("date DESC")
-        else
-          @expenses = @current_user_expenses.where('date >= ? AND date <= ?',
-                                             DateTime.now.beginning_of_month,
-                                     DateTime.now.end_of_month).order("date DESC")
-        end
-        @dates =  @current_user_expenses.all.select("date").map{ |i| i.date.month }.uniq
-        @years =  @current_user_expenses.all.select("date").map{ |i| i.date.year }.uniq
-        # binding.pry
       end
-      @payments = Payment.all
+      @dates =  @current_user_expenses.all.select("date").map{ |i| i.date.month }.uniq
+      @years =  @current_user_expenses.all.select("date").map{ |i| i.date.year }.uniq
+      # binding.pry
+    end
+    @payments = Payment.all
   end
 
   # GET /expenses/1
@@ -118,8 +118,9 @@ end
     if current_user
       @current_user_expenses =
       # this is a temporary solution to only show current year expenses.
-      current_user.expenses.where('extract(year  from date) = ?', Date.current.year)
-      # current_user.expenses.where('extract(year  from date) = ?', '2018')
+      # current_user.expenses
+      # current_user.expenses.where('extract(year  from date) = ?', Date.current.year)
+      current_user.expenses.where('extract(year  from date) = ?', '2019')
     else
       @current_user_expenses = []
     end
