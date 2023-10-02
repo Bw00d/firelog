cfceabe5b961f613575b6ff3ef9224e55a941380
Brentwood Reid <bwoodreid@gmail.com>
2023-10-01 21:17:35 -0700

    Precompile assets

--- public/assets/application-34ad5d957cf82eef5c37a38265f2b65ccc710cdbd7c4b9e413e25112235a2be8.js
+++ public/assets/application-34ad5d957cf82eef5c37a38265f2b65ccc710cdbd7c4b9e413e25112235a2be8.js
@@ -23980,6 +23980,65 @@ Copyright © 2018 Basecamp, LLC
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+$( document ).on('turbolinks:load', function(){
+
+  // Get the localStorage values
+   var category = localStorage.getItem('category-description')
+   var category_id = localStorage.getItem('category-id')
+
+  // years values are being capture in expenses.js
+   // select the category stored in localStorage
+   $("li.categories").each(function() {
+        if ($(this).text() == category) {
+          $(this).addClass('selected-category')
+        } 
+      });
+
+
+   $('li.categories').click(function(event){
+      value = $(this).closest('li').next('.hidden-id').text();
+      description = $(this).text();
+      localStorage.setItem('category-id', value)
+      localStorage.setItem('category-description', description)
+      $('.categories').removeClass('selected-category');
+      $(this).addClass('selected-category');
+
+      event.preventDefault();
+
+      if (localStorage.getItem('category-id') != "") {
+        $('#category-field').val(localStorage.getItem('category-id'));
+      } 
+        $('#search-form').submit();
+    });
+
+
+   // years
+   $('.years').click(function(event){
+    $('li.years').removeClass('selected-year');
+    $(this).addClass('selected-year');
+    event.preventDefault();
+   });
+
+
+
+   // retrieving expenses by year
+   $('li.years').click(function(event){
+      localStorage.setItem('chart-year', $(this).val())
+      $('#year-field').val(localStorage.getItem('year'));
+      
+     if (!$('#category-field').val() === '') {
+        $('#search-form').submit();
+      }
+      event.preventDefault;
+   });
+
+
+$(document).on("change", "select#category", function(e){
+   $(this).attr("expense_vendor_id", $(this).val()); // this sets the "data-option-value" to the value
+
+  });
+
+});
 $(document).on('turbolinks:load', function(){
   $('#new-expenditure-button').click(function() {
     $('#expenditure-form').show();
@@ -24015,6 +24074,8 @@ $( document ).on('turbolinks:load', function(){
 		  $(this).toggleClass('unselected');
 
 		  $('#category-field').val(value);
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
 		  event.preventDefault();
 		});
 
@@ -24078,7 +24139,12 @@ $( document ).on('turbolinks:load', function(){
    $('li.months').click(function(event){
       localStorage.setItem('month', $(this).val())
       $('#month-field').val(localStorage.getItem('month'));
-      $('#year-field').val(localStorage.getItem('year'));
+      if (localStorage.getItem('year') != "") {
+        $('#year-field').val(localStorage.getItem('year'));
+      }
+      else {
+        $('#year-field').val(0);
+      }
       $('#search-form').submit();
       event.preventDefault;
    });
@@ -24087,7 +24153,11 @@ $( document ).on('turbolinks:load', function(){
    $('li.years').click(function(event){
       localStorage.setItem('year', $(this).val())
       $('#year-field').val(localStorage.getItem('year'));
-      $('#month-field').val(localStorage.getItem('month'));
+      if (localStorage.getItem('month') != "") {
+        $('#month-field').val(localStorage.getItem('month'));
+      } else {
+        $('#month-field').val(0);
+      }
       $('#search-form').submit();
       event.preventDefault;
    });
@@ -24109,6 +24179,10 @@ $(document).on("change", "select#category", function(e){
 	});
 
 });
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
